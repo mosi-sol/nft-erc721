cd72cc488661ef1b678edb82eaecfda675fa1565
mosi <99150661+mosi-sol@users.noreply.github.com>
2022-07-04 16:43:04 +0430

    Create Mock.sol

---
+++ Contracts/Mock.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8;
+
+import "./ERC721.sol";
+
+// NFT MOCK
+contract NFT is ERC721 {
+    uint count;
+    constructor(
+        string memory __name,
+        string memory __symbol,
+        uint256 __globalRoyality
+    ) ERC721(__name, __symbol, __globalRoyality) {
+        count = 0;
+        __globalRoyality = 10;
+    }
+
+    // this important for market places + owner funcions
+    function supportsInterface(bytes4 interfaceId) external virtual override view returns (bool) {
+        return interfaceId == type(ERC721).interfaceId;
+    }
+
+    function mint() external payable virtual override {
+        _safeMint(_msgSender(), count);
+        unchecked { count += 1; }
+    }
+
+    function mint(string calldata _uri) external payable virtual override {
+        _safeMint(_msgSender(), count, _uri);
+        unchecked { count += 1; }
+    }
+
+    function burn(uint256 _tokenId) external payable virtual override {
+        _burn(_tokenId);
+    }
+}
